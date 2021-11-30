87e9ae69d495377ab073867af66b9944dfe1ca64
Junho Yeo <hanaro0704@gmail.com>
2021-12-01 04:55:36 +0900

    refactor: add path alias(`@` -> `src`)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { GlobalStyle } from '../components/GlobalStyle';
+import { GlobalStyle } from '@/components/GlobalStyle';
 
 function MyApp({ Component, pageProps }) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { HomePage } from '../Home/HomePage';
+import { HomePage } from '@/Home/HomePage';
 
 const Home = () => <HomePage />;
 
