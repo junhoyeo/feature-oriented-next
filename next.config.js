7a65afba749cd662cc3217d35fa2646d676d0b4a
Junho Yeo <i@junho.io>
2022-07-13 06:19:14 +0900

    Install devDependency `@next/bundle-analyzer`

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 const withSvgr = require('next-plugin-svgr');
-const { withPlugins } = require('next-composed-plugins');
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+module.exports = withSvgr({
+  reactStrictMode: true,
 });
-
-module.exports = withPlugins(
-  {
-    reactStrictMode: true,
-    compiler: {
-      styledComponents: true,
-    },
-  },
-  [withSvgr, withBundleAnalyzer],
-);
